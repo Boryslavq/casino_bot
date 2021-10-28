22ea3c147f1d414a8a1414e5a552c004a4f7cfab
Boryslavq <boryslav204@gmail.com>
2021-10-28 20:44:24 +0300

    Adding supervisoctl config

--- pkg/handlers/users/chanells.py
+++ pkg/handlers/users/chanells.py
@@ -5,6 +5,7 @@ from pkg.data.config import CHANNEL_ID
 async def reply_to_msg(message: types.Message):
     try:
         if message.sender_chat.id == CHANNEL_ID:
-            await message.reply("Текст для первого коментария в канале")
+            await message.reply(
+                "🤞Обсудить новость, блеснуть остроумием, получить билеты от модератора и тп можно в нашем чатике 👉 @BlefachChat\n🍀 Хочешь играть наши приватные турниры? Пиши мне @blefach_linksbot")
     except AttributeError:
         pass
